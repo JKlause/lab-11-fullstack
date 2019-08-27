a20880d1b91582c13c4376976c20310d13619c26
Joe Klause <joeklause@gmail.com>
2019-08-27 11:53:19 -0700

    set up server connection to database

--- data/create-tables.js
+++ data/create-tables.js
@@ -1,11 +1,11 @@
-const client = require('..lib/client');
+const client = require('../lib/client');
 
 client.connect()
     .then(() => {
         return client.query(`
             CREATE TABLE todos (
-                id SERIAL PRIMARY KEY NOT NULL
-                task VARCHAR(256) NOT NULL
+                id SERIAL PRIMARY KEY NOT NULL,
+                task VARCHAR(256) NOT NULL,
                 completed BOOLEAN NOT NULL DEFAULT FALSE
             );
         `);
