6fd319301dcbf205188671c56b3b0b25791531ff
Joe Klause <joeklause@gmail.com>
2019-08-27 23:02:40 -0700

    set up auth routes through server

--- server.js
+++ server.js
@@ -5,10 +5,32 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+// Database Client
 const client = require('./lib/client');
 
-// Database Client
-client.connect();
+//Auth
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+            SELECT id, email, has, display_name as "displayName
+            FROM users
+            WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        return client.query(`
+            INSERT into users (email, has, display_name)
+            VALUES ($1, $2, $3)
+            RETURNING id, email, display_name as "displayName";
+        `,
+        [user.email, hash, user.displayName]
+        ).then(result => result.rows[0]);
+    }
+});
 
 // Application Setup
 const app = express();
@@ -17,6 +39,11 @@ app.use(morgan('dev')); // http logging
 app.use(cors()); // enable CORS request
 app.use(express.static('public')); // enable serving files from public
 app.use(express.json()); // enable reading incoming json data
+
+//setup authentication routes
+app.use('api/auth', authRoutes);
+
+app.use('/api', ensureAuth);
 
 app.get('/api/todos', (req, res) => {
     client.query(`
