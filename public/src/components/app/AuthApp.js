7f866d99b150b4cc1447284ebd87b700c0f86c1e
Joe Klause <joeklause@gmail.com>
2019-08-28 10:57:00 -0700

    set up auth page

---
+++ public/src/components/app/AuthApp.js
@@ -0,0 +1,43 @@
+import Component from '../Component.js';
+import Header from './Header.js';
+import SignUp from '../auth/SignUp.js';
+import { userSignUp, userSignIn} from '../../services/todo-api.js';
+
+class AuthApp extends Component {
+
+    onRender(dom) {
+        const header = new Header();
+        dom.prepend(header.renderDOM());
+
+        const signUpContainer = dom.querySelector('#signup-container');
+        const signUp = new SignUp({
+            onSignUp: newUser => {
+                errors.textContent = '';
+
+                return 
+            }
+        }
+    }
+
+    renderHTML() {
+        return /*html*/`
+            <div>
+                <main>
+                    <p class="errors"></p>
+                    <section class="no-display" id="signup-container">
+                        <p class="switch">
+                            <button id="signin-button">Already a User?</button>
+                        </p>
+                    </section>
+                    <section id="signin-container">
+                        <p class="switch">
+                            <button id="signup-button">Create A New Account</button>
+                        </p>
+                    </section>
+                </main>
+            </div>
+        `;
+    }
+}
+
+export default AuthApp;
